a2976be922078aed770382c797db6cce963896c4
malu <maluisa.sert@hotmail.com>
2018-09-24 22:30:48 -0300

    Testando funcionalidades

--- main.c
+++ main.c
@@ -11,34 +11,38 @@ int main(){
 	MATRIZ_ESPARSA *produto;
 
 
-matriz=criar_matriz(2,2);
-set_matriz(matriz,1,1,5);
-set_matriz(matriz,1,2,1);
-set_matriz(matriz,2,1,3);
-set_matriz(matriz,2,2,2);
+	matriz=criar_matriz(2,2);
+	set_matriz(matriz,1,1,5);
+	set_matriz(matriz,1,2,1);
+	set_matriz(matriz,2,1,3);
+	set_matriz(matriz,2,2,2);
 
-printf("Matriz\n");
-print_matriz(matriz);
-printf("\n");
-matriz1=criar_matriz(2,2);
-set_matriz(matriz1,1,1,0);
-set_matriz(matriz1,1,2,5);
-set_matriz(matriz1,2,1,1);
-set_matriz(matriz1,2,2,6);
+	printf("Matriz\n");
+	print_matriz(matriz);
+	printf("\n");
+	matriz1=criar_matriz(2,2);
+	set_matriz(matriz1,1,1,0);
+	set_matriz(matriz1,1,2,5);
+	set_matriz(matriz1,2,1,1);
+	set_matriz(matriz1,2,2,6);
 
-printf("Matriz\n");
-print_matriz(matriz1);
+	printf("Matriz\n");
+	print_matriz(matriz1);
 
-printf("Transposta\n");
-transposta = transposta_matriz(matriz1);
-print_matriz(transposta);
+	printf("Transposta\n");
+	transposta = transposta_matriz(matriz1);
+	print_matriz(transposta);
 
-printf("Produto\n");
-produto = multiplicar_matriz(matriz, matriz1);
-print_matriz(produto);
+	printf("Produto\n");
+	produto = multiplicar_matriz(matriz, matriz1);
+	print_matriz(produto);
 
-soma = somar_matriz(matriz, matriz1);
-print_matriz(soma);
+	soma = somar_matriz(matriz, matriz1);
+	printf("Soma\n");
+	print_matriz(soma);
+
+	printf("matriz(2,2)\n");
+	printf("%f\n", get_matriz(matriz, 2, 2));
 
 
 }
