b96f698fbf9b086af1d4d30341b9205327e71388
PedroNatali <phrnatali@gmail.com>
2018-09-25 11:18:19 -0300

    Trabalhinho

--- main.c
+++ main.c
@@ -1,48 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matriz_esparsa.h"
+/*
+int* cria_vetor(MATRIZ_ESPARSA* matriz,int linha, int tamanho){
+		float vetor_b[tamanho];
+		for(int j = 1; j <= tamanho; j++){
+			vetor_b[j-1] = get_matriz(matriz,j,j);
+			printf("%.1f ", vetor_b[j-1]);
+		}
+		printf("\n\n");
+	}
+	*/
 
 int main(){
-    int aux;
-    MATRIZ_ESPARSA *matriz;
-	MATRIZ_ESPARSA *matriz1;
-	MATRIZ_ESPARSA *soma;
-	MATRIZ_ESPARSA *transposta;
-	MATRIZ_ESPARSA *produto;
 
+	int tamanho = 6;
+	int i;
+	MATRIZ_ESPARSA *matriz;
+	matriz = criar_matriz(6,6);
+	for(i=1; i<= tamanho; i++){
+		set_matriz(matriz,i,i,4.5);
+		set_matriz(matriz,i,i+1,-1);
+		set_matriz(matriz,i+1,i,-1);
+		set_matriz(matriz,i,i+3,-1);
+		set_matriz(matriz,i+3,i,-1); 
+	}
 
-	matriz=criar_matriz(2,2);
-	set_matriz(matriz,1,1,5);
-	set_matriz(matriz,1,2,1);
-	set_matriz(matriz,2,1,3);
-	set_matriz(matriz,2,2,2);
+	float vetor_b[tamanho];
+	for(int j = 1; j <= tamanho; j++){
+		vetor_b[j-1] = 0;
+		for(int k = 1; k<= tamanho; k++){
+			vetor_b[j-1] += get_matriz(matriz,j,k);
+		}
+		printf("%.1f ", vetor_b[j-1]);
+	}
+	printf("\n\n");
 
-	printf("Matriz\n");
-	print_matriz(matriz);
-	printf("\n");
-	matriz1=criar_matriz(2,2);
-	set_matriz(matriz1,1,1,0);
-	set_matriz(matriz1,1,2,5);
-	set_matriz(matriz1,2,1,1);
-	set_matriz(matriz1,2,2,6);
+	float vetor_x[tamanho];
 
-	printf("Matriz\n");
-	print_matriz(matriz1);
+	int j;
 
-	printf("Transposta\n");
-	transposta = transposta_matriz(matriz1);
-	print_matriz(transposta);
-
-	printf("Produto\n");
-	produto = multiplicar_matriz(matriz, matriz1);
-	print_matriz(produto);
-
-	soma = somar_matriz(matriz, matriz1);
-	printf("Soma\n");
-	print_matriz(soma);
-
-	printf("matriz(2,2)\n");
-	printf("%f\n", get_matriz(matriz, 2, 2));
-
-
+	for(i=1; i<=tamanho; i++){
+		vetor_x[i-1] = (vetor_b[i-1]/get_matriz(matriz,i,i));
+		for(j=1; j<= tamanho; j++){
+			vetor_x[j-1] -= (vetor_x[j-2]/get_matriz(matriz,j,j));
+		}
+		printf("%.3f \n\n", vetor_x[i-1]);
+		printf("%d\n", i);
+	}
 }
