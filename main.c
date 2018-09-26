f5039cb70db67cad52a7fa6386159daad708fe18
PedroNatali <phrnatali@gmail.com>
2018-09-26 07:49:36 -0300

    Implementação do exercício b completa.

--- main.c
+++ main.c
@@ -12,11 +12,19 @@ int* cria_vetor(MATRIZ_ESPARSA* matriz,int linha, int tamanho){
 	}
 	*/
 
+double modulo(double numero){
+	if(numero < 0){
+		numero = numero * (- 1);
+	}
+	return numero;
+}
+
 int main(){
 
-	int tamanho = 200;
-	int itmax = 12;
+	int tamanho = 100;
+	int itmax = 2*tamanho;
 	int i;
+	double e = 0.0000000001;
 	MATRIZ_ESPARSA *matriz;
 	matriz = criar_matriz(tamanho,tamanho);
 	for(i=1; i<= tamanho; i++){
@@ -28,46 +36,59 @@ int main(){
 	}
 
 	float vetor_b[tamanho];
-	printf("Vetor b\n");
+	//printf("Vetor b\n");
 	for(int j = 1; j <= tamanho; j++){
 		vetor_b[j-1] = 0;
 		for(int m = 1; m <= tamanho; m++){
 			vetor_b[j-1] += get_matriz(matriz,j,m);
 		}
-		printf("%.3f ", vetor_b[j-1]);
+		//printf("%.3f ", vetor_b[j-1]);
 	}
-	printf("\n\n");
+	//printf("\n\n");
 
-	printf("Vetor x zerado\n");
+	//printf("Vetor x zerado\n");
 	double vetor_x[tamanho];
 	for(int l = 0; l < tamanho; l++){
 		vetor_x[l] = 0.0;
-		printf("%.3f ", vetor_x[l]);
+		//printf("%.3f ", vetor_x[l]);
 	}
-	printf("\n\n");
+	//printf("\n\n");
 
 	int j;
-	for(int k = 0; k  < 86; k++){
-		//printf("\nk = %d ", k);
+	int k = 0;
+	double erro = 10;
+	double vetor_aux[tamanho];
+	double auxiliar1;
+	double auxiliar2;
+
+	while((k < itmax) && (erro > e)){
 		for(i = 1; i <= tamanho; i++){
-			/*for(int l = 0; l < tamanho; l++){
-				printf("%lf ", vetor_x[l]);
+			for(int p = 0; p < tamanho; p++){
+				vetor_aux[p] = vetor_x[p];
 			}
-			printf("\n");*/
 			vetor_x[i-1] = (vetor_b[i-1]/get_matriz(matriz,i,i));
 			for(int j = 1; j <= tamanho; j++){
-				//printf("j = %d %.3f ", j, vetor_x[j-i]);
 				if(i!= j){
 					vetor_x[i-1] -= (get_matriz(matriz, i, j)*vetor_x[j-1]/get_matriz(matriz,i,i));
 				}
 			}
-			//printf("\n\ni = %d -> %.6f  ", i, vetor_x[i-1]);
 		}
-		//printf("\nVetor x final\n");
-		for(int l = 0; l < tamanho; l++){
-		printf("%.2lf ", vetor_x[l]);
+		auxiliar2 = vetor_aux[0];
+		auxiliar1 = vetor_x[0];
+		for(int r = 0; r < tamanho; r++){
+			if(modulo(vetor_aux[r]) > auxiliar2){
+				auxiliar2 = modulo(vetor_aux[r]);
+			}
+			if(modulo(vetor_x[r]) > auxiliar1){
+				auxiliar1 = modulo(vetor_x[r]);
+			}
 		}
+		erro = auxiliar1 - auxiliar2;
+		k++;
 	}
-
+	printf("Vetor final: \n\n");
+	for(int l = 0; l < tamanho; l++){
+		printf("%.2lf ", vetor_x[l]);
+	}
 	printf("\n");
 }
